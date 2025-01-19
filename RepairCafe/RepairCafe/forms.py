569ebb5f20529ffde1dbb52adeed8bee888a886d
Joe Weir <2770814W@student.gla.ac.uk>
2025-01-19 16:39:36 +0000

    checkout pages and form initialised

--- RepairCafe/RepairCafe/forms.py
+++ RepairCafe/RepairCafe/forms.py
@@ -40,3 +40,120 @@ class TicketForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['readonly'] = True
+
+class CheckoutForm(forms.Form):
+    Q1 = forms.CharField(
+        label="Q1 How did you find out about the repair cafe?",
+        max_length=256,
+        required=True
+    )
+    Q2 = forms.CharField(
+        label="Q2",
+        max_length=256,
+        required=True
+    )
+
+    Q3 = forms.CharField(
+        label="Q3",
+        max_length=256,
+        required=True
+    )
+
+    Q4 = forms.CharField(
+        label="Q4",
+        max_length=256,
+        required=True
+    )
+
+    Q5 = forms.CharField(
+        label="Q5",
+        max_length=256,
+        required=True
+    )
+
+    Q6 = forms.CharField(
+        label="Q6",
+        max_length=256,
+        required=True
+    )
+
+    Q7 = forms.CharField(
+        label="Q7",
+        max_length=256,
+        required=True
+    )
+
+    Q8 = forms.CharField(
+        label="Q8",
+        max_length=256,
+        required=True
+    )
+
+    Q9 = forms.CharField(
+        label="Q9",
+        max_length=256,
+        required=True
+    )
+
+    Q10 = forms.CharField(
+        label="Q10",
+        max_length=256,
+        required=True
+    )
+
+    Q11 = forms.CharField(
+        label="Q11",
+        max_length=256,
+        required=True
+    )
+
+    Q12 = forms.CharField(
+        label="Q12",
+        max_length=256,
+        required=True
+    )
+
+
+    Q13 = forms.CharField(
+        label="Q13",
+        max_length=256,
+        required=True
+    )
+
+    Q14 = forms.CharField(
+        label="Q14",
+        max_length=256,
+        required=True
+    )
+
+    Q15 = forms.CharField(
+        label="Q15",
+        max_length=256,
+        required=True
+    )
+
+    Q16 = forms.CharField(
+        label="Q16",
+        max_length=256,
+        required=True
+    )
+
+    Q17 = forms.CharField(
+        label="Q17",
+        max_length=256,
+        required=True
+    )
+
+    Q18 = forms.CharField(
+        label="Q18",
+        max_length=256,
+        required=True
+    )
+
+    Q19 = forms.CharField(
+        label="Q19",
+        max_length=256,
+        required=True
+    )
+
+
--- RepairCafe/RepairCafe/views.py
+++ RepairCafe/RepairCafe/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import HttpResponseRedirect, render, get_object_or_404, redirect
 from .models import Ticket, Queue
-from .forms import TicketFilterForm,TicketForm,IncompleteTicketForm
+from .forms import TicketFilterForm,TicketForm,IncompleteTicketForm, CheckoutForm
 from django.urls import reverse
 from django.contrib import messages
 from django.db.models import Q
 import populate_RepairCafe as script
 from django.conf import settings
+from datetime import date
 
 
 def index(request):
@@ -138,7 +139,7 @@ def repair_item(request,repairNumber):
 
 def complete_ticket(request,repairNumber):
     ticket = Ticket.objects.get(repairNumber=repairNumber)
-    if ticket.repairStatus == 'BEING_REPAIRED' and ticket.itemCategory == "NEED_PAT":
+    if ticket.repairStatus == 'BEING_REPAIRED' and ticket.itemCategory == "ELECM":
         ticket.complete_ticket()
         messages.success(request,f"Ticket {ticket.repairNumber} - {ticket.itemName}, has been sent to PAT Testing.")
     elif(ticket.repairStatus == 'BEING_REPAIRED' ):
@@ -182,6 +183,24 @@ def enter_password(request):
 def house_rules(request):
     return render(request, 'RepairCafe/house_rules.html')
 
+def checkout(request):
+    context_dict={}
+    if request.method == 'POST':
+        form = CheckoutForm(request.POST)
+        if form.is_valid():
+            form_data = form.cleaned_data
+            form_data['event_date'] = date.today()
+            print(form_data)
+            return render(request,'RepairCafe/checkout_success.html')
+    else:
+        form=CheckoutForm
+        context_dict['form']=form
+
+    return render(request,'RepairCafe/checkout.html',context_dict)
+
+def checkout_success(request):
+    return render(request,'RepairCafe/checkout_success.html')
+        
         
 
 
