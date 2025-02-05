a7db8e61b9c6b9f423e73d63917684cf8bb50f44
2799832m <2799832m@student.gla.ac.uk>
2025-02-05 10:00:15 +0000

    percentages displayed but not styled

--- RepairCafe/RepairCafe/views.py
+++ RepairCafe/RepairCafe/views.py
@@ -103,8 +103,14 @@ def basic_stats(request):
     checkedout = Ticket.objects.filter(isCheckedOut = True).count()
     successful = Ticket.objects.filter(repairStatus = "COMPLETED").count()
     unsuccessful = Ticket.objects.filter(repairStatus = "INCOMPLETE").count()
+    catpercentages = {}
 
-    context_dict = {"checkedin":checkedin, "checkedout":checkedout, "successful":successful, "unsuccessful":unsuccessful}
+    for category in Ticket.ITEM_CATEGORY_CHOICES:
+        catpercentages[category] = ((Ticket.objects.filter(itemCategory = category[0]).count())/ (Ticket.objects.count()) * 100)
+
+    
+
+    context_dict = {"checkedin":checkedin, "checkedout":checkedout, "successful":successful, "unsuccessful":unsuccessful, "catpercentages":catpercentages}
 
     return render(request, 'RepairCafe/basic_stats.html', context_dict)
 
