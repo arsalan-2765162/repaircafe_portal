bba5721db7fb2dfc9bc7fbe65e86b6eda23b9f96
Joe Weir <2770814W@student.gla.ac.uk>
2025-02-11 17:18:50 +0000

    updated requirements.txt and pipeline to accomadete for pytests

--- RepairCafe/RepairCafe/routing.py
+++ RepairCafe/RepairCafe/routing.py
@@ -1,4 +1,7 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from django.urls import re_path
+from django.core.asgi import get_asgi_application
 from . import consumers
 
 websocket_urlpatterns = [
@@ -7,3 +10,11 @@ websocket_urlpatterns = [
     re_path(r'ws/waiting_queue/$', consumers.WaitingQueueConsumer.as_asgi()),
     re_path(r'ws/checkout_queue/$', consumers.CheckoutQueueConsumer.as_asgi()),
 ]
+
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),  
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)  
+    ),
+})
---
+++ RepairCafe/RepairCafe/test_consumers.py
@@ -0,0 +1,93 @@
+
+import pytest 
+from channels.testing import WebsocketCommunicator
+from django.conf import settings 
+from channels.layers import get_channel_layer 
+from RepairCafe.routing import application
+
+
+@pytest.mark.asyncio
+class TestConsumers: 
+    async def test_ticket_status_consumer(self):
+        communicator = WebsocketCommunicator(application,"/ws/ticket_status/123/")
+        connected, _ = await communicator.connect()
+        assert connected 
+
+        #simulating sending a ticket update
+        channel_layer = get_channel_layer()
+        await (channel_layer.group_send)(
+            "ticket_updates",
+            {
+                "type": "ticket_status_update",
+                "repairNumber": 123,
+                "status": "IN_PROGRESS",
+            },
+        )
+
+        #receive the message that is sent to the websocket
+        response = await communicator.receive_json_from()
+        assert response == {
+            "repairNumber": 123,
+            "status": "IN_PROGRESS",
+        }
+        await communicator.disconnect()
+
+    async def test_main_queue_consumer(self):
+        communicator = WebsocketCommunicator(application,"/ws/main_queue/")
+        connected, _ = await communicator.connect()
+        assert connected
+
+        #simulate sending a main queue update 
+        channel_layer = get_channel_layer()
+        await (channel_layer.group_send)(
+            "main_queue_updates",
+            {
+                "type": "queue_update",
+                "message": "ticket_added",
+            },
+        )
+
+        #receive the message that is sent to the websocket
+        response = await communicator.receive_json_from()
+        assert response == {"message": "ticket_added"}
+        await communicator.disconnect()
+    
+    async def test_waiting_queue_consumer(self):
+        communicator = WebsocketCommunicator(application, "/ws/waiting_queue/")
+        connected, _ = await communicator.connect()
+        assert connected
+
+        channel_layer = get_channel_layer()
+        await (channel_layer.group_send)(
+            "waiting_queue_updates",
+            {
+                "type": "queue_update",
+                "message": "ticket_removed",
+            },
+        )
+
+        response = await communicator.receive_json_from()
+        assert response == {"message": "ticket_removed"}
+
+        await communicator.disconnect()
+
+    async def test_checkout_queue_consumer(self):
+        communicator = WebsocketCommunicator(application, "/ws/checkout_queue/")
+        connected, _ = await communicator.connect()
+        assert connected
+
+        channel_layer = get_channel_layer()
+        await (channel_layer.group_send)(
+            "checkout_queue_updates",
+            {
+                "type": "queue_update",
+                "message": "ticket_checked_out",
+            },
+        )
+
+        response = await communicator.receive_json_from()
+        assert response == {"message": "ticket_checked_out"}
+
+        await communicator.disconnect()
+
+
