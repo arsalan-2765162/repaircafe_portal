6f22105a8b1cd5e5120ba14cd134dad5837d7c21
Joe Weir <2770814W@student.gla.ac.uk>
2025-02-25 15:15:34 +0000

    changed allowed host to new site name.

--- RepairCafe/SH28Project/settings.py
+++ RepairCafe/SH28Project/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = 't1#aj@10-snk50%a!&%@p7kh^ky&vviq87x@d48k2(n^cdvmd='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['joeweir04.pythonanywhere.com', '127.0.0.1', 'repaircafedeployed.onrender.com']
+ALLOWED_HOSTS = ['joeweir04.pythonanywhere.com', '127.0.0.1', 'mysite-9mgc.onrender.com']
 
 SITE_ID=1
 # Application definition
