eb1b941504e563e6061586cdd7e167dcc7c70dce
Joe Weir <2770814W@student.gla.ac.uk>
2025-03-22 20:16:44 +0000

    push

--- RepairCafe/SH28Project/settings.py
+++ RepairCafe/SH28Project/settings.py
@@ -122,7 +122,7 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://sh28_project_user:42JgFHcJC5k4T6es8eChEDhHysPIH2bz@dpg-cuupu3vnoe9s73am8i2g-a/sh28_project_db',
         conn_max_age=600
     )
 }
