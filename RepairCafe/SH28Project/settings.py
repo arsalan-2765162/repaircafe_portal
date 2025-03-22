45679e4d82b86bd4790815b6f2c0f0bae6d162ac
Joe Weir <2770814W@student.gla.ac.uk>
2025-03-22 16:50:20 +0000

    added static files check.

--- RepairCafe/SH28Project/settings.py
+++ RepairCafe/SH28Project/settings.py
@@ -150,9 +150,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-
-
-
 VISITOR_PRESET_PASSWORD = "visitor"
 REPAIRER_PRESET_PASSWORD = "repairer"
 VOLUNTEER_PRESET_PASSWORD = "volunteer"
@@ -179,6 +176,12 @@ STATICFILES_FINDERS = [
 ]
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #Authentication settings
 
